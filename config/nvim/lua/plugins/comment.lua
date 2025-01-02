f4097f8323432e269e3cfd1856f6728a7fa5524b
xxuxa-k <xya@tk-tech.xyz>
2025-01-02 23:17:55 +0900

    tmp commit

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -2,11 +2,11 @@ return {
   "numToStr/Comment.nvim",
   dependencies = {
     "JoosepAlviste/nvim-ts-context-commentstring",
-		config = function()
-			require("ts_context_commentstring").setup({
-				enable_autocmd = false,
-			})
-		end,
+    config = function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+    end,
   },
   event = "VeryLazy",
   config = function()
